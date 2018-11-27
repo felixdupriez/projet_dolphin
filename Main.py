61dad12944bbf7a00e388075c1f290ff32393817
etienne.legourrierec@epita.fr <9463.Bigfat253>
2018-11-27 20:02:21 +0100

    add export with ratio and all assets

--- Main.py
+++ Main.py
@@ -3,5 +3,7 @@ from Toolbox import *
 
 
 if __name__ == '__main__':
-    df = import_csv('export5')
+    df = import_csv('export6')
+    filtered_df = df[df['CLOSE_2012_01_02'].notnull()]
+    filtered_df = filtered_df.sort_values('Sharpe', ascending=False)
     print('End')
