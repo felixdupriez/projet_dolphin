676208e7543727f0f37608a2474c0248b2ee46a7
etienne.legourrierec@epita.fr <9463.Bigfat253>
2018-11-27 16:26:58 +0100

    adding close price at 2012-01-02 or 2012-01-03

--- Main.py
+++ Main.py
@@ -38,7 +38,7 @@ def get_quote(asset_id, start_date, end_date):
     stock_nav = get_data("/asset/" + asset_id + "/quote", payload=payload)
     if stock_nav == '[]':
         return
-    return json.loads(stock_nav)[0]['nav']['value']
+    return json.loads(stock_nav)[0]['close']['value']
 
 
 def get_ratio(asset_id):
@@ -50,6 +50,7 @@ def get_ratio(asset_id):
     if ratio_invoke == '[]':
         return
     return ratio_invoke
+
 
 def get_ratios(assets_id):
     sharpe_id = 20
@@ -64,13 +65,13 @@ def get_ratios(assets_id):
     return ratio_invoke
 
 
-
 def add_nav_col(df, column_name, start_date, end_date):
     df[column_name] = 0
     for index, row in df.iterrows():
-        df.loc[index, column_name] = get_quote(row.ASSET_DATABASE_ID, start_date, end_date)
+        df.loc[index, column_name] = get_quote(str(row.ASSET_DATABASE_ID), start_date, end_date)
     df[column_name] = df[column_name].str.replace(',', ".").astype(float)
     return df
+
 
 def add_ratio_col(df):
     df_size = len(df.index)
@@ -80,22 +81,40 @@ def add_ratio_col(df):
         df.loc[index, "Sharpe"] = data["20"]["value"]
         df.loc[index, "Performance"] = data["21"]["value"]
         df.loc[index, "Volatility"] = data["18"]["value"]
-        print("%d / %d" , index, df_size)
+        print("%d / %d" % (index, df_size))
+    return df
+
+
+def convert_currency(df):
+    df['CLOSE_2012_01_02'] = df['CLOSE_2012_01_02'].apply(lambda x: x * 0.88 if x.CURRENCY == 'USD' else x)
+    print('test')
     return df
 
 
 if __name__ == '__main__':
-    #result = get_data("/asset", columns=['ASSET_DATABASE_ID', 'LABEL', 'TYPE', 'MODIFICATION_DATE', 'CURRENCY'])
+    #result = get_data("/asset", columns=['ASSET_DATABASE_ID', 'LABEL', 'CURRENCY', 'TYPE', 'SUB_TYPE', 'MODIFICATION_DATE'])
     #df = json_to_data_frame(result)
     #df = df_to_value(df)
-    #df = add_nav_col(df, 'NAV_2012_01_02', '2012-01-02', '2012-01-02')
-    #df = add_nav_col(df, 'NAV_2018_11_12', '2018-11-13', '2018-11-13')
-    #df.to_csv('export', sep='\t', encoding='utf-8', index=False)      #Exporter le Dataframe en csv
-    #df = df[df.TYPE == "STOCK"]
-    df = pd.read_csv('export', sep='\t')
-    df = df[(df.NAV_2012_01_02 >= 1) & (df.NAV_2012_01_02 <= 10) & (df.TYPE == 'STOCK')]
-    df = add_ratio_col(df)
-    df.to_csv('export2', sep='\t', encoding='utf-8', index=False)
-    #df = add_nav_col(df, [18, 20, 21])
+    df = pd.read_csv('export4', sep='\t')
+    df = df[(df['CLOSE_2012_01_02'].notna()) | (df['CLOSE_2012_01_03'].notna())] #Conserver uniquement les actifs qui un prix de close
+    df['FIRST_CLOSE'] = df['CLOSE_2012_01_02']
+    for index, row in df.iterrows():
+        print(df.loc[index, "CLOSE_2012_01_02"])
+        if pd.isna(df.loc[index, "FIRST_CLOSE"]):
+            df.loc[index, "FIRST_CLOSE"] = df.loc[index, "CLOSE_2012_01_03"]
+    df = df.drop('CLOSE_2012_01_03', 1)
+    df = df.drop('CLOSE_2012_01_02', 1)
+    df.to_csv('export5', sep='\t', encoding='utf-8', index=False)
 
-    #print('test')
+    #df = add_nav_col(df, 'CLOSE_2012_01_03', '2012-01-03', '2012-01-03')
+    #df.to_csv('export4', sep='\t', encoding='utf-8', index=False)      #Exporter le Dataframe en csv
+    #df = df[df.TYPE == "STOCK"]
+    #df = convert_currency(df)
+    #df['CURRENCY'] = df.filter(like='USD', axis=1)['CURRENCY'].apply(lambda x: x * 0.88)
+    #df = df[(df.NAV_2012_01_02 >= 1) & (df.NAV_2012_01_02 <= 10)]
+    #df = df.sort_values(by=['Sharpe'], ascending=False)
+    #df = df[(df.NAV_2012_01_02 >= 1) & (df.NAV_2012_01_02 <= 10) & (df.TYPE == 'STOCK')]
+    #df = add_ratio_col(df)
+    #df.to_csv('export2', sep='\t', encoding='utf-8', index=False)
+    #df = add_nav_col(df, [18, 20, 21])
+    print('test')
