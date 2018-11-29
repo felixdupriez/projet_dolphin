c9f9a07d2711053b3134f2d186ca381b870b57cc
Theo Santos <theosnake01@gmail.com>
2018-11-29 10:37:01 +0100

    Conversion rates function

--- Conversion.py
+++ Conversion.py
@@ -2,20 +2,20 @@ import json
 import pandas as pd
 from Main import *
 
-
-def get_unique_curr(df):
+def get_unique_currs(df):
     currs = df.CURRENCY.unique()
     return currs
 
 #Taux au 2 janvier 2012
 def get_conversion_rates(currs):
-    rates = []
+    payload = {'start_date': '2012-01-02', 'end_date': '2012-01-02'}
+    rates = {}
     for curr in currs:
-        rate = get_data("/currency/" + curr + "/to/EUR")
-        rate = json.loads(rate)["rate"]["value"]
-        rates.append(rate)
+        rate = get_data("/currency/rates/" + curr + "/to/EUR", payload=payload)
+        rate = json.loads(rate)[0]["rate"]["value"]
+        rate = rate.replace(",", ".")
+        rates[curr] = float(rate)
     return rates
-
 
 def convert_to_eur(curr, amount):
     value = 0
