7d09dbe20d8e08cc5de821ebe9e54e13f1e244b8
etienne.legourrierec@epita.fr <9463.Bigfat253>
2018-11-29 17:37:46 +0100

    add magic formula

--- Correlation.py
+++ Correlation.py
@@ -1,7 +1,14 @@
-    from DataframeOperations import import_csv
+from DataframeOperations import import_csv
 
 
-if __name__ == '__main__':
-    df = import_csv('export6')
-    df.set_index('Index', inplace=True)
-    print('End')
+def magic_formula(sharpe, perf, vol):
+    return sharpe + perf - (10 * vol)
+
+
+def magic_power():
+    df = import_csv('export6', export=True)
+    df['Ponderation'] = df.apply(lambda x: magic_formula(x['Sharpe'], x['Performance'], x['Volatility']), axis=1)
+    df = df[df['CLOSE_2012_01_02'].notnull()]
+    df = df.sort_values('Ponderation', ascending=False)
+    id_list = df[0:50]['ASSET_DATABASE_ID'].tolist()
+    return id_list
