20dae3a9f375f8700b7cf22ef1e022bc2a7e332e
Theo Santos <theosnake01@gmail.com>
2018-11-29 16:20:58 +0100

    Fixed portfolio check function

--- Asset.py
+++ Asset.py
@@ -25,19 +25,9 @@ class Portfolio:
 
 def check_portfolio(portfolio):
     assets = portfolio.assets
-    min_close = assets[0].asset.get_nav()
-    max_close = assets[0].asset.get_nav()
+    if assets.amount() != 20:
+        return False
     for asset in assets:
-        if asset.get_nav() < min_close:
-            if asset.get_nav() < max_close / 10:
-                return False
-            else:
-                min_close = asset.get_nav()
-        elif asset.get_nav > max_close:
-            if asset.get_nav > 10 * min_close:
-                return False
-            else:
-                max_close = asset.get_nav
         if asset.get_nav() < (portfolio.sum / 100) or asset.get_nav() > (portfolio.sum / 10):
             return False
     return True
